cfa84602286afffb2580177a1c678fe394bef051
Michael Byrskog <mbyrskog@hotmail.com>
2025-01-27 19:06:05 +0100

    Updated about me

--- src/Routes/About.tsx
+++ src/Routes/About.tsx
@@ -5,21 +5,23 @@ const About: React.FC = () => {
     <Box>
       <Typography variant="h4">About me</Typography>
       <Divider sx={{ marginY: 2 }} />
-      <Typography>
-        Welcome to my personal portfolio! I'm Michael Byrskog, a passionate
-        professional with a solid foundation in software development and a drive
-        for leadership. My journey began as a software developer, where I honed
-        my skills in building efficient and scalable applications while
-        mastering various programming languages and technologies. Over time,
-        I've expanded my focus to include agile methodologies, embracing roles
-        that allow me to lead teams and deliver value-driven solutions. With a
-        deep understanding of both the technical and strategic aspects of
-        development, I excel at bridging the gap between stakeholders and
-        developers, ensuring seamless communication and successful outcomes. My
-        background as a developer equips me with a unique perspective, enabling
-        me to anticipate challenges, guide teams effectively, and foster
-        collaboration. Explore my portfolio to discover the projects I've worked
-        on and the skills I've refined throughout my career.
+      <Typography gutterBottom>
+        Welcome to my personal portfolio! I'm Michael Byrskog, a software
+        developer with a strong focus on building efficient and scalable
+        applications. My journey in tech has been driven by problem solving and
+        a passion for creating practical solutions. Over the years, I've worked
+        with technologies like React, .NET, and cloud based services,
+        contributing to both frontend and backend development. Beyond coding, I
+        have experience facilitating Scrum events and collaborating with teams
+        to ensure smooth development processes. I enjoy working in environments
+        where I can bridge the gap between developers and stakeholders, helping
+        to align technical solutions with business needs. Before transitioning
+        into software development, I worked with electrical installations in
+        construction sites, often under a piecework system. This experience
+        taught me the value of teamwork, precision, and efficiency, skills that
+        still influence my approach to problem solving and structured work
+        today. Feel free to explore my portfolio to see the projects I've worked
+        on and the skills I've developed throughout my career.
       </Typography>
     </Box>
   );
